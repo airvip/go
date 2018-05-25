782f36abdfc5080d3273e65157a5858d48978c7d
airvip <sdqhwzb@163.com>
2018-05-26 00:08:53 +0800

    warning

--- go02_flow_control/branch.go
+++ go02_flow_control/branch.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-	const filename  = "abc.txt"
-	contents, err := ioutil.ReadFile(filename)
+	const filename  = "E:/phpStudy/WWW/go/go02_flow_control/abc.txt"
+	b, err := ioutil.ReadFile(filename)
 	if err != nil{
 		fmt.Println(err)
 	}else {
-		fmt.Printf("%s\n", contents)
+		fmt.Printf("%s\n", b)
 	}
 }
