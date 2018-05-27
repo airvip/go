9598afa77ab6a687a38bf8632c4bcca9a933b772
airvip <sdqhwzb@163.com>
2018-05-27 19:03:57 +0800

    switch

--- go02_flow_control/branch.go
+++ go02_flow_control/branch.go
@@ -24,7 +24,52 @@ func read2()  {
 	}
 }
 
+func eval(a, b int, op string) int {
+	var result int
+	switch op {
+	case "+":
+		result = a + b
+	case "-":
+		result = a - b
+	case "*":
+		result = a * b
+	case "/":
+		result = a / b
+	default:
+		panic("unsupported operator:" + op)
+	}
+	return result
+}
+
+func grade(score int) string {
+	g := ""
+	switch  {
+	case score < 0 || score > 100:
+		panic("illegal score")
+	case score > 90:
+		g = "A"
+	case score > 80:
+		g = "B"
+	case score > 70:
+		g = "C"
+	case score > 60:
+		g = "D"
+	default:
+		g = "E"
+	}
+	return g
+}
+
 func main() {
-	read1()
-	read2()
+	//read1()
+	//read2()
+	fmt.Println(eval(10, 2, "/"))
+	//println(eval(10, 2, "/"))
+	//println(eval(10, 2, "%"))
+	fmt.Println(
+		grade(45),
+		grade(67),
+		grade(78),
+		grade(89),
+		grade(100))
 }
