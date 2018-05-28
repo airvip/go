1848ca7b6b03a3bd31a75643f1706523217d9040
airvip <sdqhwzb@163.com>
2018-05-29 00:54:00 +0800

    add all

--- go03_func/func.go
+++ go03_func/func.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+	"math"
+)
 
 func eval(a, b int, op string) int {
 	switch op {
@@ -27,9 +32,23 @@ func div2(a, b int) (q, r int) {
 	return a / b, a % b
 }
 
+func apply(op func(int, int) int, a, b, int) int {
+	p := reflect.ValueOf(op).Pointer()
+    opName := runtime.FuncForPC(p).Name()
+	fmt.Printf("Calling function %s with args" +
+		"(%d, %d)", opName, a, b)
+    return op(a ,b)
+}
+
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
 func main() {
 	fmt.Println(eval(20, 5, "/"))
 	fmt.Println(div(13, 5))
 	q, r := div2(13, 5)
 	fmt.Println(q, r)
+
+	fmt.Println(apply(pow, 3, 4))
 }
