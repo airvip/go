a5d7a5a30434d504cb75087deb60446aac1abd3d
airvip <sdqhwzb@163.com>
2018-06-04 23:55:07 +0800

    add *

--- oop/node.go
+++ oop/node.go
@@ -7,9 +7,19 @@ type treeNode struct {
 	left, right *treeNode
 }
 
+func (node treeNode) print() {
+	fmt.Print(node.value)
+}
+
+func (node *treeNode) setValue(value int) {
+    node.value = value
+}
+
 func createNode(value int) *treeNode {
     return &treeNode{value:value}
 }
+
+
 
 func main() {
 	var root treeNode
@@ -17,13 +27,19 @@ func main() {
 	root = treeNode{value:3}
 	root.left = &treeNode{}
 	root.right = &treeNode{5, nil, nil}
-	//root.right.left = new(treeNode)
-	root.right.left = createNode(2)
+	root.right.left = new(treeNode)
+	root.left.right = createNode(2)
 
-	nodes := []treeNode{
+    root.print()
+	fmt.Println()
+	root.right.left.setValue(4)
+	root.right.left.print()
+	fmt.Println()
+
+	/*nodes := []treeNode{
 		{value:3},
 		{},
 		{6, nil, &root},
 	}
-	fmt.Println(nodes)
+	fmt.Println(nodes)*/
 }
