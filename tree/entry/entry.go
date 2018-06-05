abb478165f7634eecd39e2d1ea50a344b7133c61
airvip <sdqhwzb@163.com>
2018-06-06 00:28:14 +0800

    tree

--- tree/entry/entry.go
+++ tree/entry/entry.go
@@ -1,10 +1,35 @@
 package main
 
-import "go/tree"
+import (
+	"go/tree"
+	"fmt"
+)
+
+type myTreeNode struct{
+	node *tree.Node
+}
+
+func (myNode *myTreeNode) postOrder() {
+    if myNode == nil || myNode.node == nil{
+    	return
+	}
+
+	left := myTreeNode{myNode.node.Left}
+	right := myTreeNode{myNode.node.Right}
+	left.postOrder()
+	right.postOrder()
+	myNode.node.Print()
+}
+/*
+     3
+   /   \
+  0     5
+   \   /
+    2 0
+*/
 
 func main() {
 	var root tree.Node
-
 
 	root = tree.Node{Value:3}
 	root.Left = &tree.Node{}
@@ -13,5 +38,9 @@ func main() {
 	root.Left.Right = tree.CreateNode(2)
 	root.Right.Left.SetValue(4)
 	root.Traverse()
+	fmt.Println()
+	myRoot := myTreeNode{&root}
+	myRoot.postOrder()
+	fmt.Println()
 
 }
